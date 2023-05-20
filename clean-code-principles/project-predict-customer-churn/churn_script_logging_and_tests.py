c7d15d7306b083a597680f4b2b56310343e378d6
Stan Taov <stastaov@gmail.com>
2023-05-20 13:52:38 -0400

    fix: refactor churn_script_logging_and_tests.py script

--- clean-code-principles/project-predict-customer-churn/churn_script_logging_and_tests.py
+++ clean-code-principles/project-predict-customer-churn/churn_script_logging_and_tests.py
@@ -6,9 +6,9 @@ unit tests for the churn_library.py functions
 
 Author: Stan Taov
 
-Date: May 17, 2022
+Date: May 20, 2022
 
-Version: 0.0.1
+Version: 0.0.2
 """
 import unittest
 import logging
@@ -18,19 +18,33 @@ import pandas as pd
 import toml
 from churn_library import import_data, perform_eda, encoder_helper, perform_feature_engineering, train_models
 
-logging.basicConfig(
-    filename='./logs/churn_library.log',
-    level=logging.INFO,
-    filemode='w',
-    format='%(name)s - %(levelname)s - %(message)s')
+# logging.basicConfig(
+#     filename='./logs/churn_library.log',
+#     level=logging.INFO,
+#     filemode='w',
+#     format='%(name)s - %(levelname)s - %(message)s')
 
+# create logger
+logger = logging.getLogger('churn_test')
+logger.setLevel(logging.DEBUG)
 
+# create console handler and set level to debug
+ch = logging.FileHandler('./logs/churn_test.log', 'w', 'utf-8')
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# loading some default variables from toml file
 config = toml.load("config.toml")
 
-path = config['PATH']
+path = config['DATA_PATH']
 category_columns = config['CATEGORY_COLUMNS']
 eda_image_paths = config['EDA_IMAGE_PTHS']
 model_image_paths = config['MODEL_IMAGE_PTHS']
+plot_columns = config['PLOT_COLUMNS']
 
 class TestChurn(unittest.TestCase):
     def setUp(self) -> None:
@@ -38,75 +52,75 @@ class TestChurn(unittest.TestCase):
             self.df = import_data(path)
             self.df['Churn'] = self.df['Attrition_Flag'].apply(
                 lambda val: 0 if val == "Existing Customer" else 1)
-            logging.info("Testing import_data: SUCCESS")
+            logger.info("Testing import_data: SUCCESS")
         except FileNotFoundError as err:
-            logging.error("ERROR: The input file is missing")
+            logger.error("The input file is missing")
             raise err
 
 
-    def test_import(self):
-        '''
-        test data import - this example is completed for you to assist with the other test functions
-        '''
-        logging.info("Testing test_import function")
+    def test_import(self) -> None:
+        """
+        test the data 
+        """
+        logger.info("Testing test_import function")
 
         try:
             assert self.df.shape[0] > 0
             assert self.df.shape[1] > 0
         except AssertionError as err:
-            logging.error("ERROR: Dataframe is missing some data")
+            logger.error("DataFrame is missing some data")
             raise err
 
 
-    def test_eda(self):
+    def test_eda(self) -> None:
         '''
         test perform eda function
         '''
-        logging.info("Testing perform_eda function")
+        logger.info("Testing perform_eda function")
 
-        perform_eda(self.df)
+        perform_eda(self.df, plot_columns)
 
         image_pths = glob.glob('images/eda/*.png')
 
         try:
             for pth in eda_image_paths:
                 assert pth in image_pths
-                logging.info("All required EDA images were created: SUCCESS")
+                logger.info("All required EDA images were created: SUCCESS")
         except AssertionError as err:
-            logging.error(
-                'ERROR: Required EDA image %s was not created by perform_eda() function',
+            logger.error(
+                'Required EDA image %s was not created by perform_eda() function',
                 pth)
             raise err
 
 
-    def test_encoder_helper(self):
+    def test_encoder_helper(self) -> None:
         '''
         test encoder helper
         '''
-        logging.info("Testing encoder_helper function")
+        logger.info("Testing encoder_helper function")
 
         try:
             print(self.df)
             self.encoded_df = encoder_helper(self.df, category_columns, "Churn")
         except AssertionError as err:
-            logging.error(
-                "ERROR: Some function arguments were not provided")
+            logger.error(
+                "Some function arguments were not provided")
             raise err
 
         for col in category_columns:
             try:
                 assert f"{col}_Churn" in self.encoded_df.columns
-                logging.info("%s column was created: SUCCESS", col)
+                logger.info("%s column was created: SUCCESS", col)
             except AssertionError as err:
-                logging.error("ERROR: %s column is missing", col)
+                logger.error("ERROR: %s column is missing", col)
                 raise err
 
 
-    def test_perform_feature_engineering(self):
+    def test_perform_feature_engineering(self) -> None:
         '''
         test perform_feature_engineering
         '''
-        logging.info("Testing perform_feature_engineering")
+        logger.info("Testing perform_feature_engineering")
         
         self.encoded_df = encoder_helper(
             df=self.df,
@@ -117,8 +131,8 @@ class TestChurn(unittest.TestCase):
             X_train, X_test, y_train, y_test = perform_feature_engineering(
                 self.encoded_df, response='Churn')
         except TypeError as err:
-            logging.error(
-                "ERROR: Incorrect input dataframe was proviced")
+            logger.error(
+                "Incorrect input dataframe was proviced")
             raise err
 
         try:
@@ -128,17 +142,17 @@ class TestChurn(unittest.TestCase):
             assert isinstance(X_test, pd.DataFrame)
             assert isinstance(y_train, pd.Series)
             assert isinstance(y_test, pd.Series)
-            logging.info("Train/Test data were created: SUCCESS")
+            logger.info("Train/Test data were created: SUCCESS")
         except AssertionError as err:
-            logging.error("ERROR: Train/Test data has wrong data format")
+            logger.error("Train/Test data has wrong data format")
             raise err
 
 
-    def test_train_models(self):
+    def test_train_models(self) -> None:
         '''
         test perform_feature_engineering
         '''
-        logging.info("Testing train_models function")
+        logger.info("Testing train_models function")
         
         self.encoded_df = encoder_helper(
             df=self.df,
@@ -150,7 +164,7 @@ class TestChurn(unittest.TestCase):
         try:
             train_models(X_train, X_test, y_train, y_test)
         except NameError as err:
-            logging.error(
+            logger.error(
                 "ERROR: You haven't defined one of the required args in train_models()")
             raise err
 
@@ -160,7 +174,7 @@ class TestChurn(unittest.TestCase):
             for pth in model_image_paths:
                 assert pth in image_pths
         except AssertionError as err:
-            logging.error(
+            logger.error(
                 'ERROR: Something went wrong, not all model images were generated, %s',
                 pth)
             raise err
@@ -170,7 +184,7 @@ class TestChurn(unittest.TestCase):
             joblib.load('models/logistic_model.pkl')
             logging.info("Testing testing_models: SUCCESS")
         except FileNotFoundError as err:
-            logging.error("ERROR: Some models were not generated")
+            logger.error("ERROR: Some models were not generated")
             raise err
 
 
